aae7c220a53b4b3217aa87497cb0b92ceef44c2a
Eduardoo142 <earangos142@gmail.com>
2022-10-31 16:36:59 -0500

    CorreoQR

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 import re
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 
@@ -11,6 +14,14 @@ def login_view(request):
    
         username = request.POST['username']
         password = request.POST['password']
+
+        subject="Verifica tu cuenta"
+        message= "Por favor verifica tu cuenta leyendo este código QR: "
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [username]
+
+        send_mail(subject, message, email_from, recipient_list)
+
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
