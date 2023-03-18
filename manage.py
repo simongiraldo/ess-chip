542f8460a7edfa671695ec73f1f00634c6ee02be
Susana Uribe Velásquez <susyuribe22@gmail.com>
2023-03-18 18:48:32 -0500

    Comentarios del manage.py
    Jajaja

--- manage.py
+++ manage.py
@@ -20,3 +20,29 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+#Ahora una pequena documentacion de como funciona el archivo manage.py
+"""
+Este archivo es el que se encarga de ejecutar las tareas administrativas de django, como por ejemplo:
+    - Crear un proyecto
+    - Crear una aplicacion
+    - Migrar la base de datos
+    - Correr el servidor de desarrollo
+    - Correr los test
+    - etc
+
+Para ejecutar una tarea se debe ejecutar el siguiente comando:
+    python manage.py <nombre de la tarea>
+    
+Ejemplo:
+    python manage.py runserver
+    python manage.py createsuperuser
+    python manage.py startapp <nombre de la aplicacion>
+    python manage.py makemigrations
+    python manage.py migrate
+    python manage.py test
+    etc
+
+Para ver todas las tareas que se pueden ejecutar con el comando manage.py se debe ejecutar el siguiente comando:
+    python manage.py help
+"""
